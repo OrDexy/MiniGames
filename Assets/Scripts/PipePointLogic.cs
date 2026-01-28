2d30081c500a9e217be92f4cf2834fb9417d4355
OrDexy <kireevdaniil@outlook.com>
2026-01-28 12:43:18 +0300

    Pipe Fix with logic and try SO(Linux)

--- Assets/Scripts/PipePointLogic.cs
+++ Assets/Scripts/PipePointLogic.cs
@@ -10,17 +10,12 @@ public class PipePointLogic : MonoBehaviour
 
     public Sprite online;
     public Sprite offline;
+    
     void Start() 
     { 
         sr = pipe.GetComponent<SpriteRenderer>();
     }
-    void Update() 
-    {
-        foreach (var item in friends)
-        {
-            if (item.gameObject.tag != "On") gameObject.tag = "Untagged";
-        }
-    }
+    
     void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "On")
@@ -29,21 +24,22 @@ public class PipePointLogic : MonoBehaviour
             foreach (var item in friends)
             {
                 item.gameObject.tag = "On";
-                gameObject.tag = "On";
             }
-        }
-    }
-    void OnTriggerExit2D(Collider2D collision)
-    {
-
-        if (collision.gameObject.tag == "On")
-        {
-            sr.sprite = offline;
+        } else {
+		      sr.sprite = offline;
             foreach (var item in friends)
             {
                 item.gameObject.tag = "Untagged";
-                gameObject.tag = "Untagged";
-            }
+            }        
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        sr.sprite = offline;
+        foreach (var item in friends)
+        {
+            item.gameObject.tag = "Untagged";
         }
     }
 }
---
+++ Assets/Scripts/ScriptableScenario.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Scenario", menuName = "ScriptableObjects/PipeScenarioScriptableObject", order = 1)]
+public class PipeScenarioScriptableObject : ScriptableObject
+{
+	 public int countOfLine;
+	 public int countOfTurn;
+	 public int countOfTShape;
+	 
+   	
+}
