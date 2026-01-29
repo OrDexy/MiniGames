9cec59ff072a84bc520a3f6b563679b333e733ce
OrDexy <kireevdaniil@outlook.com>
2026-01-29 10:09:59 +0300

    Pipe spawn and try DnD(Linux)

--- Assets/Scripts/PipeG.cs
+++ Assets/Scripts/PipeG.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
-
+//rtrtrt
 public class PipeG : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+	 public PipeScenarioScriptableObject[] scenes;
+	 
+	 public float maxX;
+	 public float maxY;
+	 
+	 public GameObject line;
+	 public GameObject turn;
+	 public GameObject tShape;
+	 public GameObject inP;
+	 public GameObject outP;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        int rnd = Random.Range(0, scenes.Length);
+        inP.transform.Translate(new Vector3(0, scenes[rnd].yInput, 0));
+        outP.transform.Translate(new Vector3(0, scenes[rnd].yOutput, 0));
+        for(int i = 0; i < scenes[rnd].countOfLine; i++)
+        	   Instantiate(line, new Vector2(Random.Range(-maxX, maxX), Random.Range(-maxY, maxY)), Quaternion.identity);
+        for(int i = 0; i < scenes[rnd].countOfTurn; i++)
+        	   Instantiate(turn, new Vector2(Random.Range(-maxX, maxX), Random.Range(-maxY, maxY)), Quaternion.identity);
+        for(int i = 0; i < scenes[rnd].countOfTShape; i++)
+        	   Instantiate(tShape, new Vector2(Random.Range(-maxX, maxX), Random.Range(-maxY, maxY)), Quaternion.identity);
         
     }
 }
--- Assets/Scripts/ScriptableScenario.cs
+++ Assets/Scripts/ScriptableScenario.cs
@@ -5,6 +5,7 @@ public class PipeScenarioScriptableObject : ScriptableObject
 	 public int countOfLine;
 	 public int countOfTurn;
 	 public int countOfTShape;
-	 
-   	
+	 //0 out of 10
+    public int yInput;
+    public int yOutput;	
 }
